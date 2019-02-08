0ffadd248c18df448638ed7cd389e97c1150c3b3
GriffinHealy <tug14984@temple.edu>
2019-02-08 11:23:37 -0500

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Griffin Healy on 2/8/19.
+//  Copyright © 2019 Griffin Healy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
